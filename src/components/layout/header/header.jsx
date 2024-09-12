0913a400bb1ba6ef298c39eab1ed0404064ca132
Fatal999 <euronymous.ts@yandex.ru>
2024-09-12 16:39:59 +0300

    v1.0

--- src/components/layout/header/header.jsx
+++ src/components/layout/header/header.jsx
@@ -3,7 +3,7 @@ import "../header/header.scss"
 export default function Header() {
   return (
     <header>
-      <h1>Header</h1>
+      <h1>Письмо в будущее</h1>
     </header>
   )
 }
--- src/components/layout/main/main.jsx
+++ src/components/layout/main/main.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react"
+
 export default function Main() {
+  const [checkActive, setCheckActive] = useState(false)
+
+  function checkChange() {
+    checkActive === false ? setCheckActive(true) : setCheckActive(false)
+  }
+
   return (
-    <header>
-      <h1>Main</h1>
-    </header>
+    <main>
+      <h2>Отправь письмо себе или близкому челу в будущее!!!</h2>
+      <label>
+        Уже <input type="number" value={0} readOnly></input> человек отправили письмо
+      </label>
+      <form action="https://echo.htmlacademy.ru/" method="post">
+        <textarea placeholder="Пиши тут" name="mail"></textarea>
+        <input type="date" name="date"></input>
+        <input type="email" name="email" placeholder="Введи свою почту"></input>
+        <label htmlFor="agree">
+          <input type="checkbox" id="agree" name="agree" checked={checkActive} value={checkActive} onChange={checkChange}></input>
+          <span></span>Я согласен с <button type="button">условиями</button>
+        </label>
+        <button type="submit" disabled={checkActive === false}>
+          Отправить
+        </button>
+      </form>
+    </main>
   )
 }
