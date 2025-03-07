11319e31d2dc9537439ac0ab57b2548d945907ca
Fatal999 <euronymous.ts@yandex.ru>
2025-03-07 15:56:32 +0300

    v3.0

--- src/js/public-messages.js
+++ src/js/public-messages.js
@@ -1,6 +1,9 @@
 const publicButton = document.getElementById("public-button");
 const headerWrapper = document.querySelector("header");
 const url = "https://letter2future.ru/api/public-emails/";
+let nextUrl = "";
+let dataNext;
+let prevUrl = "";
 
 function publicMessages() {
   publicButton.blur();
@@ -32,7 +35,10 @@ function publicMessages() {
     "bg-white",
     "rounded-[25px]",
     "w-[320px]",
-    "h-[500px]",
+    "max-h-[80vh]",
+    "overflow-auto",
+    "hide-scrollbar",
+    "p-[10px]",
   );
 
   messageArea.appendChild(messageAreaOverlay);
@@ -40,11 +46,59 @@ function publicMessages() {
 
   headerWrapper.appendChild(messageArea);
 
+  const buttonWrapper = document.createElement("div");
+  buttonWrapper.classList.add("flex", "w-[320px]", "justify-around");
+
+  const prevButton = document.createElement("button");
+  prevButton.innerHTML = "<";
+  prevButton.classList.add(
+    "flex",
+    "justify-center",
+    "items-center",
+    "font-mono",
+    "cursor-pointer",
+    "color-black",
+    "bg-transparent",
+    "border-[2px]",
+    "w-[30px]",
+    "h-[30px]",
+    "rounded-[25px]",
+  );
+  prevButton.disabled = "true";
+
+  const nextButton = document.createElement("button");
+  nextButton.innerHTML = ">";
+  nextButton.classList.add(
+    "flex",
+    "justify-center",
+    "items-center",
+    "font-mono",
+    "cursor-pointer",
+    "color-black",
+    "bg-transparent",
+    "border-[2px]",
+    "w-[30px]",
+    "h-[30px]",
+    "rounded-[25px]",
+  );
+
+  messageAreaWrapper.appendChild(buttonWrapper);
+
+  buttonWrapper.appendChild(prevButton);
+  buttonWrapper.appendChild(nextButton);
+
   function createMessages(data) {
     data.results.forEach((el) => {
       const messageText = document.createElement("p");
       messageText.textContent = el.text;
+      messageText.classList.add("font-mono", "text-center");
       messageAreaWrapper.appendChild(messageText);
+    });
+  }
+
+  function cleanMessages() {
+    messageAreaWrapper.querySelectorAll("p").forEach((el) => {
+      el.remove();
     });
   }
 
@@ -61,10 +115,68 @@ function publicMessages() {
       return response.json();
     })
     .then((data) => {
-      console.log("Server:", data);
       createMessages(data);
+      nextUrl = data.next;
+      prevUrl = data.previous;
+      dataNext = data;
     })
     .catch((error) => console.error("Ошибка запроса:", error));
+
+  function nextMessagePage() {
+    if (dataNext.next !== null) {
+      fetch(nextUrl, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            console.log("Error", response.status);
+          }
+          return response.json();
+        })
+        .then((data) => {
+          cleanMessages();
+          createMessages(data);
+          nextUrl = data.next;
+          prevUrl = data.previous;
+          dataNext = data;
+
+          nextButton.disabled = !data.next;
+          prevButton.disabled = !data.previous;
+        })
+        .catch((error) => console.error("Ошибка запроса:", error));
+    }
+  }
+
+  function prevMessagePage() {
+    if (dataNext.previous !== null) {
+      fetch(prevUrl, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            console.log("Error", response.status);
+          }
+          return response.json();
+        })
+        .then((data) => {
+          cleanMessages();
+          createMessages(data);
+          nextUrl = data.next;
+          prevUrl = data.previous;
+          dataNext = data;
+
+          nextButton.disabled = !data.next;
+          prevButton.disabled = !data.previous;
+        })
+        .catch((error) => console.error("Ошибка запроса:", error));
+    }
+  }
 
   function closeMessagePopup() {
     if (headerWrapper.contains(messageArea)) {
@@ -73,6 +185,9 @@ function publicMessages() {
       headerWrapper.removeChild(messageArea);
     }
   }
+
+  nextButton.addEventListener("click", nextMessagePage);
+  prevButton.addEventListener("click", prevMessagePage);
 
   messageAreaOverlay.addEventListener("click", closeMessagePopup);
 
